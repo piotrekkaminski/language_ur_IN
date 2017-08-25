555eb3322be717ba14fa7f756c242da48c0f220d
Piotr Kaminski <magentopeter@gmail.com>
2017-08-25 15:04:49 -0500

    Translation autoupdate from Crowdin for ur_IN - 2017-08-25 20:04:49 branch Head

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magentotwotranslations_ur_in',
+    __DIR__
+);
